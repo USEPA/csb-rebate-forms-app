1157fe5a9b420da24cedc534011a7f5ea9057096
Courtney Myers <courtney.myers@erg.com>
2022-03-21 15:38:13 -0400

    Simplify Login component markup

--- app/client/src/components/login.tsx
+++ app/client/src/components/login.tsx
@@ -18,46 +18,44 @@ export default function Login() {
   const destination = (location.state as LocationState).redirectedFrom || "/";
 
   return (
-    <div className="margin-top-2 bg-base-lightest">
-      <div className="padding-9 text-center">
-        <button
-          className="usa-button font-sans-2xs"
-          onClick={(ev) => {
-            dispatch({ type: "FETCH_EPA_USER_DATA_REQUEST" });
+    <div className="padding-9 text-center bg-base-lightest">
+      <button
+        className="usa-button margin-0 font-sans-2xs"
+        onClick={(ev) => {
+          dispatch({ type: "FETCH_EPA_USER_DATA_REQUEST" });
 
-            fetchData(`${apiUrl}/api/v1/login`)
-              .then((loginRes) => {
-                dispatch({
-                  type: "FETCH_EPA_USER_DATA_SUCCESS",
-                  payload: { epaUserData: loginRes },
-                });
-
-                dispatch({ type: "USER_SIGN_IN" });
-
-                // NOTE: { replace: true } passed so an entry for "/login"
-                // isn't added to the history stack, so the user can click
-                // the back button and not go back to this login page
-                navigate(destination, { replace: true });
-              })
-              .catch((loginErr) => {
-                console.error("Error fetching EPA user data");
-                dispatch({ type: "FETCH_EPA_USER_DATA_FAILURE" });
+          fetchData(`${apiUrl}/api/v1/login`)
+            .then((loginRes) => {
+              dispatch({
+                type: "FETCH_EPA_USER_DATA_SUCCESS",
+                payload: { epaUserData: loginRes },
               });
-          }}
-        >
-          <span className="display-flex flex-align-center">
-            <span className="margin-right-1">Sign in</span>
-            <svg
-              className="usa-icon"
-              aria-hidden="true"
-              focusable="false"
-              role="img"
-            >
-              <use href={`${icons}#login`} />
-            </svg>
-          </span>
-        </button>
-      </div>
+
+              dispatch({ type: "USER_SIGN_IN" });
+
+              // NOTE: { replace: true } passed so an entry for "/login"
+              // isn't added to the history stack, so the user can click
+              // the back button and not go back to this login page
+              navigate(destination, { replace: true });
+            })
+            .catch((loginErr) => {
+              console.error("Error fetching EPA user data");
+              dispatch({ type: "FETCH_EPA_USER_DATA_FAILURE" });
+            });
+        }}
+      >
+        <span className="display-flex flex-align-center">
+          <span className="margin-right-1">Sign in</span>
+          <svg
+            className="usa-icon"
+            aria-hidden="true"
+            focusable="false"
+            role="img"
+          >
+            <use href={`${icons}#login`} />
+          </svg>
+        </span>
+      </button>
     </div>
   );
 }
