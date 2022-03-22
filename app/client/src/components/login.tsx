0d18d3f247dc4883c1a6eb3b30f5ef6d2da7b9a6
Devin <devin.galloway@erg.com>
2022-03-21 18:42:46 -0600

    Change login route to link to server SAML login instead of button for API calls

--- app/client/src/components/login.tsx
+++ app/client/src/components/login.tsx
@@ -1,63 +1,13 @@
-import { useLocation, useNavigate } from "react-router-dom";
 import icons from "uswds/img/sprite.svg";
 // ---
-import { useApiState, fetchData } from "contexts/api";
-import { useUserDispatch } from "contexts/user";
-
-type LocationState = {
-  redirectedFrom: string;
-};
 
 export default function Login() {
-  const location = useLocation();
-  const navigate = useNavigate();
-  const { apiUrl } = useApiState();
-  const dispatch = useUserDispatch();
-
-  // page user was previously on before they were redirected to "/login"
-  const destination = (location.state as LocationState).redirectedFrom || "/";
-
   return (
     <div className="margin-top-2 bg-base-lightest">
       <div className="padding-9 text-center">
-        <button
+        <a
           className="usa-button font-sans-2xs"
-          onClick={(ev) => {
-            dispatch({ type: "FETCH_EPA_USER_DATA_REQUEST" });
-
-            fetchData(`${apiUrl}/api/v1/login`)
-              .then((loginRes) => {
-                dispatch({
-                  type: "FETCH_EPA_USER_DATA_SUCCESS",
-                  payload: { epaUserData: loginRes },
-                });
-
-                dispatch({ type: "FETCH_SAM_USER_DATA_REQUEST" });
-
-                fetchData(`${apiUrl}/api/v1/bap`)
-                  .then((bapRes) => {
-                    dispatch({
-                      type: "FETCH_SAM_USER_DATA_SUCCESS",
-                      payload: { samUserData: bapRes },
-                    });
-
-                    dispatch({ type: "USER_SIGN_IN" });
-
-                    // NOTE: { replace: true } passed so an entry for "/login"
-                    // isn't added to the history stack, so the user can click
-                    // the back button and not go back to this login page
-                    navigate(destination, { replace: true });
-                  })
-                  .catch((bapErr) => {
-                    console.error("Error fetching SAM user data");
-                    dispatch({ type: "FETCH_SAM_USER_DATA_FAILURE" });
-                  });
-              })
-              .catch((loginErr) => {
-                console.error("Error fetching EPA user data");
-                dispatch({ type: "FETCH_EPA_USER_DATA_FAILURE" });
-              });
-          }}
+          href={`${process.env.REACT_APP_SERVER_URL}/login`}
         >
           <span className="display-flex flex-align-center">
             <span className="margin-right-1">Sign in</span>
@@ -70,7 +20,7 @@ export default function Login() {
               <use href={`${icons}#login`} />
             </svg>
           </span>
-        </button>
+        </a>
       </div>
     </div>
   );
