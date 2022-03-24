c8dcfb2daf0305e86c7cbf6f02937ace94feacb8
Courtney Myers <courtney.myers@erg.com>
2022-03-24 09:15:28 -0400

    Update axios variables to not shadow express route variables (e.g. res -> axiosRes), and update axios import so it includes ts typings

--- app/server/app/routes/api.js
+++ app/server/app/routes/api.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const axios = require("axios");
+const axios = require("axios").default;
 const { ensureAuthenticated } = require("../middleware");
 
 const router = express.Router();
 
 const formioProjectUrl = process.env.FORMIO_PROJECT_URL;
 const formioFormId = process.env.FORMIO_FORM_ID;
-const fetchOptions = { headers: { "x-token": process.env.FORMIO_API_KEY } };
+const formioHeaders = { headers: { "x-token": process.env.FORMIO_API_KEY } };
 
 router.get("/user", ensureAuthenticated, function (req, res) {
   // Call BAP/SAM API for UEI data to add onto user data
@@ -35,8 +35,8 @@ router.get("/rebate-form-submissions", ensureAuthenticated, (req, res) => {
   // const url = `${formioBaseUrl}/submission?data.uei=${query}`;
 
   axios
-    .get(`${formioProjectUrl}/${formioFormId}/submission`, fetchOptions)
-    .then((res) => res.data)
+    .get(`${formioProjectUrl}/${formioFormId}/submission`, formioHeaders)
+    .then((axiosRes) => axiosRes.data)
     .then((submissions) => {
       return submissions.map((sub) => {
         const { _id, _fid, form, project, created, modified, data } = sub;
@@ -57,13 +57,13 @@ router.get("/rebate-form-submissions", ensureAuthenticated, (req, res) => {
       });
     })
     .then((submissions) => res.json(submissions))
-    .catch((err) => {
-      if (typeof err.toJSON === "function") {
-        console.error(err.toJSON());
+    .catch((error) => {
+      if (typeof error.toJSON === "function") {
+        console.error(error.toJSON());
       }
 
       res
-        .status(err?.response?.status || 500)
+        .status(error?.response?.status || 500)
         .json({ message: "Error fetching Forms.gov submissions" });
     });
 });
@@ -72,12 +72,12 @@ router.get("/rebate-form-submission/:id", ensureAuthenticated, (req, res) => {
   const id = req.params.id;
 
   axios
-    .get(`${formioProjectUrl}/${formioFormId}/submission/${id}`, fetchOptions)
-    .then((res) => res.data)
+    .get(`${formioProjectUrl}/${formioFormId}/submission/${id}`, formioHeaders)
+    .then((axiosRes) => axiosRes.data)
     .then((submission) => {
       axios
-        .get(`${formioProjectUrl}/form/${submission.form}`, fetchOptions)
-        .then((res) => res.data)
+        .get(`${formioProjectUrl}/form/${submission.form}`, formioHeaders)
+        .then((axiosRes) => axiosRes.data)
         .then((schema) => {
           res.json({
             jsonSchema: schema,
@@ -85,13 +85,13 @@ router.get("/rebate-form-submission/:id", ensureAuthenticated, (req, res) => {
           });
         });
     })
-    .catch((err) => {
-      if (typeof err.toJSON === "function") {
-        console.error(err.toJSON());
+    .catch((error) => {
+      if (typeof error.toJSON === "function") {
+        console.error(error.toJSON());
       }
 
       res
-        .status(err?.response?.status || 500)
+        .status(error?.response?.status || 500)
         .json({ message: `Error fetching Forms.gov submission ${id}` });
     });
 });
