ebb4f4f17d6dc6c9cd230be986c72b8c82b800ef
Courtney Myers <courtney.myers@erg.com>
2022-03-27 10:47:28 -0400

    Enable and disable dashboard buttons/links based on whether user is on dashboard page or not

--- app/client/src/components/dashboard.tsx
+++ app/client/src/components/dashboard.tsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 import { Formio } from "formiojs";
 import uswds from "@formio/uswds";
 import icons from "uswds/img/sprite.svg";
@@ -8,7 +8,30 @@ import { useUserState } from "contexts/user";
 
 Formio.use(uswds);
 
+function AllRebatesText() {
+  return (
+    <span className="display-flex flex-align-center">
+      <svg className="usa-icon" aria-hidden="true" focusable="false" role="img">
+        <use href={`${icons}#list`} />
+      </svg>
+      <span className="margin-left-1">All Rebates</span>
+    </span>
+  );
+}
+
+function NewRebateText() {
+  return (
+    <span className="display-flex flex-align-center">
+      <svg className="usa-icon" aria-hidden="true" focusable="false" role="img">
+        <use href={`${icons}#add_circle`} />
+      </svg>
+      <span className="margin-left-1">New Rebate</span>
+    </span>
+  );
+}
+
 export default function Dashboard() {
+  const { pathname } = useLocation();
   const { userData } = useUserState();
 
   return (
@@ -16,35 +39,27 @@ export default function Dashboard() {
       <h1>Clean School Bus Rebate Forms</h1>
 
       <div className="display-flex flex-justify border-bottom padding-bottom-1">
-        <nav>
-          <Link to="/" className="usa-button font-sans-2xs">
-            <span className="display-flex flex-align-center">
-              <svg
-                className="usa-icon"
-                aria-hidden="true"
-                focusable="false"
-                role="img"
-              >
-                <use href={`${icons}#list`} />
-              </svg>
-              <span className="margin-left-1">All Rebates</span>
-            </span>
-          </Link>
+        {pathname === "/" ? (
+          <nav>
+            <button className="usa-button font-sans-2xs" disabled>
+              <AllRebatesText />
+            </button>
 
-          <Link to="/rebate/new" className="usa-button font-sans-2xs">
-            <span className="display-flex flex-align-center">
-              <svg
-                className="usa-icon"
-                aria-hidden="true"
-                focusable="false"
-                role="img"
-              >
-                <use href={`${icons}#add_circle`} />
-              </svg>
-              <span className="margin-left-1">New Rebate</span>
-            </span>
-          </Link>
-        </nav>
+            <Link to="/rebate/new" className="usa-button font-sans-2xs">
+              <NewRebateText />
+            </Link>
+          </nav>
+        ) : (
+          <nav>
+            <Link to="/" className="usa-button font-sans-2xs">
+              <AllRebatesText />
+            </Link>
+
+            <button className="usa-button font-sans-2xs" disabled>
+              <NewRebateText />
+            </button>
+          </nav>
+        )}
 
         <nav className="display-flex flex-align-center">
           <p className="margin-bottom-0 margin-right-1">
