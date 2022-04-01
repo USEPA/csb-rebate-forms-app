c297d8313378041a716ad56aa196b9dfd98afc1d
Devin <devin.galloway@erg.com>
2022-03-31 22:04:40 -0600

    Move SAM data query to separate API endpoint to avoid jwt cookie being too large

--- app/server/app/routes/api.js
+++ app/server/app/routes/api.js
@@ -2,8 +2,13 @@ const { resolve } = require("node:path");
 const { readFile } = require("node:fs/promises");
 const express = require("express");
 const axios = require("axios").default;
+const jsforce = require("jsforce");
 // ---
 const { ensureAuthenticated } = require("../middleware");
+const logger = require("../utilities/logger");
+const { createJwt } = require("../utilities/createJwt");
+
+const log = logger.logger;
 
 const router = express.Router();
 
@@ -16,13 +21,93 @@ const formioApiKey = process.env.FORMIO_API_KEY;
 
 const formioHeaders = { headers: { "x-token": formioApiKey } };
 
+const getSamData = (email) => {
+  const conn = new jsforce.Connection({
+    oauth2: {
+      loginUrl: process.env.BAP_URL,
+      clientId: process.env.BAP_CLIENT_ID,
+      clientSecret: process.env.BAP_CLIENT_SECRET,
+      redirectUri: process.env.SERVER_URL,
+    },
+  });
+
+  return conn
+    .login(process.env.BAP_USER, process.env.BAP_PASSWORD)
+    .then(() => {
+      // After successful login, query for SAM data
+      return conn
+        .query(
+          `
+            SELECT
+                ENTITY_COMBO_KEY__c,
+                ENTITY_STATUS__c,
+                UNIQUE_ENTITY_ID__c,
+                ENTITY_EFT_INDICATOR__c,
+                CAGE_CODE__c,
+                NAME,
+                GOVT_BUS_POC_NAME__c,
+                GOVT_BUS_POC_EMAIL__c,
+                ALT_GOVT_BUS_POC_NAME__c,
+                ALT_GOVT_BUS_POC_EMAIL__c,
+                ELEC_BUS_POC_NAME__c,
+                ELEC_BUS_POC_EMAIL__c,
+                ALT_ELEC_BUS_POC_NAME__c,
+                ALT_ELEC_BUS_POC_EMAIL__c,
+                PHYSICAL_ADDRESS_LINE_1__c,
+                PHYSICAL_ADDRESS_LINE_2__c,
+                PHYSICAL_ADDRESS_CITY__c,
+                PHYSICAL_ADDRESS_PROVINCE_OR_STATE__c,
+                PHYSICAL_ADDRESS_ZIPPOSTAL_CODE__c,
+                PHYSICAL_ADDRESS_ZIP_CODE_4__c
+            FROM ${process.env.BAP_TABLE}
+            WHERE
+                ALT_ELEC_BUS_POC_EMAIL__c = '${email}' or
+                GOVT_BUS_POC_EMAIL__c = '${email}' or
+                ALT_GOVT_BUS_POC_EMAIL__c = '${email}' or
+                ELEC_BUS_POC_EMAIL__c = '${email}'
+          `
+        )
+        .then((res) => {
+          return res.records;
+        })
+        .catch((err) => {
+          log.error(err);
+          throw err;
+        });
+    })
+    .catch((err) => {
+      log.error(err);
+      throw err;
+    });
+};
+
 router.use(ensureAuthenticated);
 
-router.get("/user", function (req, res) {
-  // TODO: map through samUserData to clean up field names if needed
-
-  // req.user includes both epaUserData (object) and samUserData (array)
+router.get("/user", (req, res) => {
   res.json(req.user);
+});
+
+router.get("/sam-data", (req, res) => {
+  getSamData(req.user.mail)
+    .then((samUserData) => {
+      // First check if user has at least one associated UEI before completing login process
+      if (samUserData && !samUserData.length) {
+        log.error(
+          `User ${req.user.mail} tried to use app without any associated SAM records`
+        );
+        return res
+          .status(401)
+          .json({ message: "No SAM data available for user." });
+      }
+
+      res.json(samUserData);
+    })
+    .catch((err) => {
+      log.error(err);
+      res
+        .status(401)
+        .json({ message: "There was an error retrieving SAM data." });
+    });
 });
 
 // TODO: Add log info when admin/helpdesk changes submission back to draft
--- app/server/app/routes/auth.js
+++ app/server/app/routes/auth.js
@@ -4,7 +4,6 @@ const samlStrategy = require("../config/samlStrategy");
 const { ensureAuthenticated } = require("../middleware");
 const { createJwt } = require("../utilities/createJwt");
 const logger = require("../utilities/logger");
-const jsforce = require("jsforce");
 
 const log = logger.logger;
 
@@ -12,67 +11,6 @@ const router = express.Router();
 
 // For redirects below, set const for base url (SERVER_URL is needed as fallback when using sub path, e.g. /csb)
 const baseUrl = process.env.CLIENT_URL || process.env.SERVER_URL;
-
-const getSamData = (email) => {
-  const conn = new jsforce.Connection({
-    oauth2: {
-      // you can change loginUrl to connect to sandbox or prerelease env.
-      loginUrl: process.env.BAP_URL,
-      clientId: process.env.BAP_CLIENT_ID,
-      clientSecret: process.env.BAP_CLIENT_SECRET,
-      redirectUri: process.env.SERVER_URL,
-    },
-  });
-
-  return conn
-    .login(process.env.BAP_USER, process.env.BAP_PASSWORD)
-    .then(() => {
-      // After successful login, query for SAM data
-      return conn
-        .query(
-          `
-            SELECT
-                ENTITY_COMBO_KEY__c,
-                ENTITY_STATUS__c,
-                UNIQUE_ENTITY_ID__c,
-                ENTITY_EFT_INDICATOR__c,
-                CAGE_CODE__c,
-                NAME,
-                GOVT_BUS_POC_NAME__c,
-                GOVT_BUS_POC_EMAIL__c,
-                ALT_GOVT_BUS_POC_NAME__c,
-                ALT_GOVT_BUS_POC_EMAIL__c,
-                ELEC_BUS_POC_NAME__c,
-                ELEC_BUS_POC_EMAIL__c,
-                ALT_ELEC_BUS_POC_NAME__c,
-                ALT_ELEC_BUS_POC_EMAIL__c,
-                PHYSICAL_ADDRESS_LINE_1__c,
-                PHYSICAL_ADDRESS_LINE_2__c,
-                PHYSICAL_ADDRESS_CITY__c,
-                PHYSICAL_ADDRESS_PROVINCE_OR_STATE__c,
-                PHYSICAL_ADDRESS_ZIPPOSTAL_CODE__c,
-                PHYSICAL_ADDRESS_ZIP_CODE_4__c
-            FROM ${process.env.BAP_TABLE}
-            WHERE
-                ALT_ELEC_BUS_POC_EMAIL__c = '${email}' or
-                GOVT_BUS_POC_EMAIL__c = '${email}' or
-                ALT_GOVT_BUS_POC_EMAIL__c = '${email}' or
-                ELEC_BUS_POC_EMAIL__c = '${email}'
-          `
-        )
-        .then((res) => {
-          return res.records;
-        })
-        .catch((err) => {
-          log.error(err);
-          throw err;
-        });
-    })
-    .catch((err) => {
-      log.error(err);
-      throw err;
-    });
-};
 
 // TODO: pass RelayState from front-end if necessary?
 router.get(
@@ -93,36 +31,25 @@ router.post(
   (req, res) => {
     const epaUserData = req.user.attributes;
 
-    getSamData(req.user.mail)
-      .then((samUserData) => {
-        // First check if user has at least one associated UEI before completing login process
-        if (samUserData && !samUserData.length) {
-          res.redirect(`${baseUrl}/welcome?error=uei`);
-        }
+    // Create JWT, set as cookie, then redirect to client
+    const token = createJwt(epaUserData);
+    res.cookie("token", token, { httpOnly: true });
 
-        // Create JWT, set as cookie, then redirect to client
-        const token = createJwt({ epaUserData, samUserData });
-        res.cookie("token", token, { httpOnly: true });
+    // If user has Admin or Helpdesk role, log to INFO
+    log.info(
+      `User with email ${epaUserData.mail} and member of ${
+        epaUserData.memberof || "no"
+      } groups logged in.`
+    );
 
-        // If user has Admin or Helpdesk role, log to INFO
-        log.info(
-          `User with email ${epaUserData.mail} and member of ${
-            epaUserData.memberof || "no"
-          } groups logged in.`
-        );
-
-        // "RelayState" will be the path that the user initially tried to access before being sent to /login
-        res.redirect(`${baseUrl}${req.body.RelayState || "/"}`);
-      })
-      .catch((err) => {
-        log.error(err);
-        res.redirect(`${baseUrl}/welcome?error=bap`);
-      });
+    // "RelayState" will be the path that the user initially tried to access before being sent to /login
+    res.redirect(`${baseUrl}${req.body.RelayState || "/"}`);
   }
 );
 
 router.get("/login/fail", (req, res) => {
-  res.status(401).json({ message: "Login failed" });
+  log.error("SAML login failed");
+  res.redirect(`${baseUrl}/welcome?error=auth`);
 });
 
 router.get("/logout", ensureAuthenticated, (req, res) => {
