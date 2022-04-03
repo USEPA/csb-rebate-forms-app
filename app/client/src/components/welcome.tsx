7e631de0a1ff9ee9aac3ef44f974c4ceb54ff5d1
Courtney Myers <courtney.myers@erg.com>
2022-04-03 14:19:57 -0400

    Update Welcome component to display error messages, based on 'error' query string param passed by server app (currently for auth and saml errors)

--- app/client/src/components/welcome.tsx
+++ app/client/src/components/welcome.tsx
@@ -1,7 +1,9 @@
-import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLocation, useSearchParams } from "react-router-dom";
 import icons from "uswds/img/sprite.svg";
 // ---
 import { serverUrl } from "../config";
+import Message from "components/message";
 
 type LocationState = {
   redirectedFrom: string;
@@ -9,6 +11,23 @@ type LocationState = {
 
 export default function Welcome() {
   const location = useLocation();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    if (!searchParams.has("error")) return;
+
+    if (searchParams.get("error") === "auth") {
+      setErrorMessage("Auth error message here"); // TODO: update message
+    }
+
+    if (searchParams.get("error") === "saml") {
+      setErrorMessage("SAML error message here"); // TODO: update message
+    }
+
+    setSearchParams("");
+  }, [searchParams, setSearchParams]);
 
   // page user was previously on before they were redirected to "/welcome"
   const destination = (location.state as LocationState)?.redirectedFrom || "/";
@@ -19,28 +38,32 @@ export default function Welcome() {
   console.log("previous url", destination);
 
   return (
-    <div className="padding-9 border-1px border-base-lighter text-center bg-base-lightest">
-      <p>
-        Click the <strong>Sign in</strong> button below to log into the{" "}
-        <em>Clean School Bus Rebate Forms</em> application.
-      </p>
+    <>
+      {errorMessage && <Message type="error" text={errorMessage} />}
 
-      <a
-        className="usa-button margin-top-1 margin-right-0 font-sans-2xs"
-        href={`${serverUrl}/login`}
-      >
-        <span className="display-flex flex-align-center">
-          <span className="margin-right-1">Sign in</span>
-          <svg
-            className="usa-icon"
-            aria-hidden="true"
-            focusable="false"
-            role="img"
-          >
-            <use href={`${icons}#login`} />
-          </svg>
-        </span>
-      </a>
-    </div>
+      <div className="padding-9 border-1px border-base-lighter text-center bg-base-lightest">
+        <p>
+          Click the <strong>Sign in</strong> button below to log into the{" "}
+          <em>Clean School Bus Rebate Forms</em> application.
+        </p>
+
+        <a
+          className="usa-button margin-top-1 margin-right-0 font-sans-2xs"
+          href={`${serverUrl}/login`}
+        >
+          <span className="display-flex flex-align-center">
+            <span className="margin-right-1">Sign in</span>
+            <svg
+              className="usa-icon"
+              aria-hidden="true"
+              focusable="false"
+              role="img"
+            >
+              <use href={`${icons}#login`} />
+            </svg>
+          </span>
+        </a>
+      </div>
+    </>
   );
 }
