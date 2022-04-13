1401bc16062a48cdd847709586102618e9b6bf4f
Courtney Myers <courtney.myers@erg.com>
2022-04-13 14:45:04 -0400

    Remove react-router navigate state from client app, as server app is setup to handle redirected an unauthenticated user to the page they attempted to navigate to after they've been logged in

--- app/client/src/components/welcome.tsx
+++ app/client/src/components/welcome.tsx
@@ -1,16 +1,11 @@
 import { useEffect } from "react";
-import { useLocation, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import icons from "uswds/img/sprite.svg";
 // ---
 import { serverUrl } from "../config";
 import Message, { useMessageState } from "components/message";
 
-type LocationState = {
-  redirectedFrom: string;
-};
-
 export default function Welcome() {
-  const location = useLocation();
   const [searchParams, setSearchParams] = useSearchParams();
 
   const {
@@ -67,14 +62,6 @@ export default function Welcome() {
     displaySuccessMessage,
     displayErrorMessage,
   ]);
-
-  // page user was previously on before they were redirected to "/welcome"
-  const destination = (location.state as LocationState)?.redirectedFrom || "/";
-
-  // TODO: append the destination url to the login link's href as a query string
-  // param, so it could be used in the server app's /login controller
-  // (change the `successRedirect` value of '/' to the destination url)
-  console.log("previous url", destination);
 
   return (
     <>
