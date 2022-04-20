341ec333c2e6a43e5cd86d1f8220450713b1314f
Courtney Myers <courtney.myers@erg.com>
2022-04-20 18:01:24 -0400

    Update axiosFormio interceptors with logic to retry requests (thanks to axios-retry)

--- app/server/app/config/formio.js
+++ app/server/app/config/formio.js
@@ -1,4 +1,8 @@
 const axios = require("axios").default;
+// ---
+const logger = require("../utilities/logger");
+
+const log = logger.logger;
 
 const formioProjectUrl = process.env.FORMIO_PROJECT_URL;
 const formioFormId = process.env.FORMIO_FORM_ID;
@@ -6,7 +10,10 @@ const formioApiKey = process.env.FORMIO_API_KEY;
 
 const axiosFormio = axios.create();
 
+// NOTE: thanks to https://github.com/softonic/axios-retry for the retry logic
 axiosFormio.interceptors.request.use((config) => {
+  config.csb = config.csb || {};
+  config.csb.retryCount = config.csb.retryCount || 0;
   config.headers["x-token"] = formioApiKey;
   return config;
 });
@@ -14,7 +21,27 @@ axiosFormio.interceptors.request.use((config) => {
 axiosFormio.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (typeof error.toJSON === "function") log.debug(error.toJSON());
+    if (typeof error.toJSON === "function") {
+      log.debug(error.toJSON());
+    }
+
+    // attempt to retry a failed request two more times, and log the attempts
+    const { config } = error;
+
+    if (config.csb.retryCount < 2) {
+      config.csb.retryCount += 1;
+
+      log.warn(
+        `${error.response.status} ${config.method.toUpperCase()} ${
+          config.url
+        } – Retrying (${config.csb.retryCount} of 2)...`
+      );
+
+      return new Promise((resolve) =>
+        setTimeout(() => resolve(axiosFormio.request(config)), 1000)
+      );
+    }
+
     return Promise.reject(error);
   }
 );
