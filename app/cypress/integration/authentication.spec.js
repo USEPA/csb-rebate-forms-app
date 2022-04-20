d2b6838e5920fd2c731cb6bf841881724d9fe1a6
Caleb Schwind <46329268+cschwinderg@users.noreply.github.com>
2022-04-20 11:01:44 -0400

    Fixed the authentication tests.

--- app/cypress/integration/authentication.spec.js
+++ app/cypress/integration/authentication.spec.js
@@ -13,6 +13,11 @@ describe('Authentication', () => {
     cy.loginToCSB('csbtest');
   });
 
+  const loggedOutMessage = 'You have succesfully logged out.';
+  const samInfoText =
+    'No SAM.gov records match your email. Only Government and Electronic Business SAM.gov Points of Contacts (and alternates) may edit and submit Clean School Bus Rebate Forms.';
+  const samErrorText = 'Error retrieving SAM.gov data. Please contact support.';
+
   it('Sign in to CSB', () => {
     // verify the user name is displayed on the screen
     cy.findByText('csb-test@erg.com');
@@ -26,7 +31,7 @@ describe('Authentication', () => {
     cy.findByText('Sign out').click();
 
     // verify sign out was completed
-    cy.findByText('You have succesfully logged out.');
+    cy.findByText(loggedOutMessage);
     cy.contains(
       'Click the Sign in button below to login to the Clean School Bus Rebate Dashboard using Login.gov.',
     );
@@ -42,12 +47,10 @@ describe('Authentication', () => {
     cy.findByText('Error logging in. Please try again or contact support.');
 
     cy.visit('/welcome?error=sam-fetch');
-    cy.findByText('Error retrieving SAM.gov data. Please contact support.');
+    cy.findByText(samErrorText);
 
     cy.visit('/welcome?info=sam-results');
-    cy.findByText(
-      'No SAM.gov records found. Please refer to the help documentation to add data to SAM.gov.',
-    );
+    cy.findByText(samInfoText);
 
     cy.visit('/welcome?info=timeout');
     cy.findByText(
@@ -55,14 +58,14 @@ describe('Authentication', () => {
     );
 
     cy.visit('/welcome?success=logout');
-    cy.findByText('You have succesfully logged out.');
+    cy.findByText(loggedOutMessage);
   });
 
   it('Test SAM.gov service failure', () => {
     // sign out
     cy.findByText('csb-test@erg.com');
     cy.findByText('Sign out').click();
-    cy.findByText('You have succesfully logged out.');
+    cy.findByText(loggedOutMessage);
 
     // simulate the sam-data service failing
     const origin =
@@ -76,14 +79,14 @@ describe('Authentication', () => {
 
     // verify the appropriate error message is displayed
     cy.loginToCSB('csbtest');
-    cy.findByText('Error retrieving SAM.gov data. Please contact support.');
+    cy.findByText(samErrorText);
   });
 
   it('Test SAM.gov service with no results', () => {
     // sign out
     cy.findByText('csb-test@erg.com');
     cy.findByText('Sign out').click();
-    cy.findByText('You have succesfully logged out.');
+    cy.findByText(loggedOutMessage);
 
     // simulate the sam-data service with no results
     const origin =
@@ -100,8 +103,6 @@ describe('Authentication', () => {
 
     // verify the appropriate message is displayed
     cy.loginToCSB('csbtest');
-    cy.findByText(
-      'No SAM.gov records found. Please refer to the help documentation to add data to SAM.gov.',
-    );
+    cy.findByText(samInfoText);
   });
 });
