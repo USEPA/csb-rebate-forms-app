6d9bade210157cce6aa95f96e8ebffbb3cfe6051
Courtney Myers <courtney.myers@erg.com>
2022-04-23 20:54:56 -0400

    Remove useMessageState custom hook and replace use in Welcome and NewRebate components with message state

--- app/client/src/components/welcome.tsx
+++ app/client/src/components/welcome.tsx
@@ -1,67 +1,74 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import icons from "uswds/img/sprite.svg";
 // ---
 import { serverUrl } from "../config";
-import Message, { useMessageState } from "components/message";
+import Message from "components/message";
 
 export default function Welcome() {
   const [searchParams, setSearchParams] = useSearchParams();
 
-  const {
-    message,
-    displayInfoMessage,
-    displaySuccessMessage,
-    displayErrorMessage,
-  } = useMessageState();
+  const [message, setMessage] = useState<{
+    displayed: boolean;
+    type: "info" | "success" | "warning" | "error";
+    text: string;
+  }>({
+    displayed: false,
+    type: "info",
+    text: "",
+  });
 
   useEffect(() => {
     if (searchParams.get("error") === "auth") {
-      displayErrorMessage(
-        "Authentication error. Please log in again or contact support."
-      );
-      setSearchParams("");
+      setMessage({
+        displayed: true,
+        type: "error",
+        text: "Authentication error. Please log in again or contact support.",
+      });
     }
 
     if (searchParams.get("error") === "saml") {
-      displayErrorMessage(
-        "Error logging in. Please try again or contact support."
-      );
-      setSearchParams("");
+      setMessage({
+        displayed: true,
+        type: "error",
+        text: "Error logging in. Please try again or contact support.",
+      });
     }
 
     if (searchParams.get("error") === "sam-fetch") {
-      displayErrorMessage(
-        "Error retrieving SAM.gov data. Please contact support."
-      );
-      setSearchParams("");
+      setMessage({
+        displayed: true,
+        type: "error",
+        text: "Error retrieving SAM.gov data. Please contact support.",
+      });
     }
 
     if (searchParams.get("info") === "sam-results") {
-      displayInfoMessage(
-        "No SAM.gov records match your email. Only Government and Electronic Business SAM.gov Points of Contacts (and alternates) may edit and submit Clean School Bus Rebate Forms."
-      );
-      setSearchParams("");
+      setMessage({
+        displayed: true,
+        type: "info",
+        text: "No SAM.gov records match your email. Only Government and Electronic Business SAM.gov Points of Contacts (and alternates) may edit and submit Clean School Bus Rebate Forms.",
+      });
     }
 
     if (searchParams.get("info") === "timeout") {
-      displayInfoMessage(
-        "For security reasons, you have been logged out due to 15 minutes of inactivity."
-      );
-      setSearchParams("");
+      setMessage({
+        displayed: true,
+        type: "info",
+        text: "For security reasons, you have been logged out due to 15 minutes of inactivity.",
+      });
     }
 
     if (searchParams.get("success") === "logout") {
-      displaySuccessMessage("You have succesfully logged out.");
-      setSearchParams("");
+      setMessage({
+        displayed: true,
+        type: "success",
+        text: "You have succesfully logged out.",
+      });
     }
-  }, [
-    searchParams,
-    setSearchParams,
-    displayInfoMessage,
-    displaySuccessMessage,
-    displayErrorMessage,
-  ]);
+
+    setSearchParams("");
+  }, [searchParams, setSearchParams]);
 
   return (
     <>
