c7c5d2e3ff7c8d9ef256c4904707277d0b55d8a0
Courtney Myers <courtney.myers@erg.com>
2022-06-23 15:25:07 -0400

    Inject currently logged in user's email, title, and name in hidden form fields for new and existing rebate forms

--- app/client/src/routes/newRebate.tsx
+++ app/client/src/routes/newRebate.tsx
@@ -4,52 +4,25 @@ import { DialogOverlay, DialogContent } from "@reach/dialog";
 import icons from "uswds/img/sprite.svg";
 // ---
 import { serverUrl, fetchData } from "../config";
+import { getUserInfo } from "../utilities";
 import Loading from "components/loading";
 import Message from "components/message";
 import MarkdownContent from "components/markdownContent";
 import { TextWithTooltip } from "components/infoTooltip";
 import { useContentState } from "contexts/content";
-import { EPAUserData, SAMUserData, useUserState } from "contexts/user";
+import { SamEntityData, useUserState } from "contexts/user";
 
-function getMatchedContactInfo(samData: SAMUserData, epaData: EPAUserData) {
-  const samEmailFields = [
-    "ELEC_BUS_POC_EMAIL__c",
-    "ALT_ELEC_BUS_POC_EMAIL__c",
-    "GOVT_BUS_POC_EMAIL__c",
-    "ALT_GOVT_BUS_POC_EMAIL__c",
-  ];
-
-  let matchedEmailField;
-
-  for (const [field, value] of Object.entries(samData)) {
-    if (!samEmailFields.includes(field)) continue;
-    // NOTE: take the first match only – there shouldn't be a case where the
-    // currently logged in user would be listed as multiple POCs for a single record
-    if (
-      typeof value === "string" &&
-      value.toLowerCase() === epaData.mail.toLowerCase()
-    ) {
-      matchedEmailField = field;
-      break;
-    }
-  }
-
-  const fieldPrefix = matchedEmailField?.split("_EMAIL__c").shift();
-
-  return {
-    title: samData[`${fieldPrefix}_TITLE__c` as keyof SAMUserData] as string,
-    name: samData[`${fieldPrefix}_NAME__c` as keyof SAMUserData] as string,
-  };
-}
-
-function createNewRebate(samData: SAMUserData, epaData: EPAUserData) {
-  const { title, name } = getMatchedContactInfo(samData, epaData);
+function createNewRebate(email: string, samData: SamEntityData) {
+  const { title, name } = getUserInfo(email, samData);
 
   return fetchData(`${serverUrl}/api/rebate-form-submission/`, {
     data: {
-      last_updated_by: epaData.mail,
+      last_updated_by: email,
+      hidden_current_user_email: email,
+      hidden_current_user_title: title,
+      hidden_current_user_name: name,
       bap_hidden_entity_combo_key: samData.ENTITY_COMBO_KEY__c,
-      sam_hidden_applicant_email: epaData.mail,
+      sam_hidden_applicant_email: email,
       sam_hidden_applicant_title: title,
       sam_hidden_applicant_name: name,
       sam_hidden_applicant_efti: samData.ENTITY_EFT_INDICATOR__c,
@@ -80,14 +53,15 @@ export default function NewRebate() {
     text: "",
   });
 
-  const activeSamData =
-    samUserData.status === "success" &&
-    samUserData.data?.results &&
-    samUserData.data?.records.filter((e) => e.ENTITY_STATUS__c === "Active");
-
-  if (epaUserData.status !== "success") {
-    return null;
+  if (epaUserData.status !== "success" || samUserData.status !== "success") {
+    return <Loading />;
   }
+
+  const email = epaUserData.data.mail;
+
+  const activeSamRecords = samUserData.data.records.filter((record) => {
+    return record.ENTITY_STATUS__c === "Active";
+  });
 
   return (
     <div className="margin-top-2">
@@ -99,135 +73,134 @@ export default function NewRebate() {
         >
           <div className="usa-modal__content">
             <div className="usa-modal__main">
-              {content.status === "success" && (
-                <MarkdownContent
-                  className="margin-top-4"
-                  children={content.data?.newRebateDialog || ""}
-                  components={{
-                    h2: (props) => (
-                      <h2
-                        id="csb-new-rebate-modal-heading"
-                        className="usa-modal__heading text-center"
-                      >
-                        {props.children}
-                      </h2>
-                    ),
-                    p: (props) => (
-                      <p
-                        id="csb-new-rebate-modal-description"
-                        className="text-center"
-                      >
-                        {props.children}
-                      </p>
-                    ),
-                  }}
+              {activeSamRecords.length <= 0 ? (
+                <Message
+                  type="info"
+                  text="No SAM.gov records match your email. Only Government and Electronic Business SAM.gov Points of Contacts (and alternates) may edit and submit Clean School Bus Rebate Forms."
                 />
-              )}
+              ) : (
+                <>
+                  {content.status === "success" && (
+                    <MarkdownContent
+                      className="margin-top-4"
+                      children={content.data?.newRebateDialog || ""}
+                      components={{
+                        h2: (props) => (
+                          <h2
+                            id="csb-new-rebate-modal-heading"
+                            className="usa-modal__heading text-center"
+                          >
+                            {props.children}
+                          </h2>
+                        ),
+                        p: (props) => (
+                          <p
+                            id="csb-new-rebate-modal-description"
+                            className="text-center"
+                          >
+                            {props.children}
+                          </p>
+                        ),
+                      }}
+                    />
+                  )}
 
-              {message.displayed && (
-                <Message type={message.type} text={message.text} />
-              )}
+                  {message.displayed && (
+                    <Message type={message.type} text={message.text} />
+                  )}
 
-              <div className="usa-table-container--scrollable" tabIndex={0}>
-                <table 
-                  aria-label="SAM.gov Entities"
-                  className="usa-table usa-table--stacked usa-table--borderless usa-table--striped width-full"
-                >
-                  <thead>
-                    <tr className="font-sans-2xs text-no-wrap">
-                      <th scope="col">
-                        <span className="usa-sr-only">Create</span>
-                      </th>
-                      <th scope="col">
-                        <TextWithTooltip
-                          text="UEI"
-                          tooltip="Unique Entity ID from SAM.gov"
-                        />
-                      </th>
-                      <th scope="col">
-                        <TextWithTooltip
-                          text="EFT Indicator"
-                          tooltip="Electronic Funds Transfer Indicator listing the associated bank account from SAM.gov"
-                        />
-                      </th>
-                      <th scope="col">
-                        <TextWithTooltip
-                          text="Applicant"
-                          tooltip="Legal Business Name from SAM.gov for this UEI"
-                        />
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {!activeSamData ? (
-                      <tr>
-                        <td colSpan={4}>
-                          <div className="margin-bottom-2">
-                            <Loading />
-                          </div>
-                        </td>
-                      </tr>
-                    ) : (
-                      activeSamData.map((samData, index) => (
-                        <tr key={index}>
-                          <th scope="row" className="font-sans-2xs">
-                            <button
-                              className="usa-button font-sans-2xs margin-right-0 padding-x-105 padding-y-1"
-                              onClick={(ev) => {
-                                setMessage({
-                                  displayed: true,
-                                  type: "info",
-                                  text: "Creating new rebate form application...",
-                                });
-
-                                createNewRebate(samData, epaUserData.data)
-                                  .then((res) => {
-                                    navigate(`/rebate/${res._id}`);
-                                  })
-                                  .catch((err) => {
-                                    setMessage({
-                                      displayed: true,
-                                      type: "error",
-                                      text: "Error creating new rebate form application.",
-                                    });
-                                  });
-                              }}
-                            >
-                              <span className="usa-sr-only">
-                                Create Form with UEI:{" "}
-                                {samData.UNIQUE_ENTITY_ID__c} and EFTI:{" "}
-                                {samData.ENTITY_EFT_INDICATOR__c}
-                              </span>
-                              <span className="display-flex flex-align-center">
-                                <svg
-                                  className="usa-icon"
-                                  aria-hidden="true"
-                                  focusable="false"
-                                  role="img"
-                                >
-                                  <use href={`${icons}#arrow_forward`} />
-                                </svg>
-                                <span className="mobile-lg:display-none margin-left-1">
-                                  New Form
-                                </span>
-                              </span>
-                            </button>
+                  <div className="usa-table-container--scrollable" tabIndex={0}>
+                    <table
+                      aria-label="SAM.gov Entities"
+                      className="usa-table usa-table--stacked usa-table--borderless usa-table--striped width-full"
+                    >
+                      <thead>
+                        <tr className="font-sans-2xs text-no-wrap">
+                          <th scope="col">
+                            <span className="usa-sr-only">Create</span>
                           </th>
-                          <td className="font-sans-2xs">
-                            {samData.UNIQUE_ENTITY_ID__c}
-                          </td>
-                          <td className="font-sans-2xs">
-                            {samData.ENTITY_EFT_INDICATOR__c}
-                          </td>
-                          <td className="font-sans-2xs">
-                            {samData.LEGAL_BUSINESS_NAME__c}
-                          </td>
+                          <th scope="col">
+                            <TextWithTooltip
+                              text="UEI"
+                              tooltip="Unique Entity ID from SAM.gov"
+                            />
+                          </th>
+                          <th scope="col">
+                            <TextWithTooltip
+                              text="EFT Indicator"
+                              tooltip="Electronic Funds Transfer Indicator listing the associated bank account from SAM.gov"
+                            />
+                          </th>
+                          <th scope="col">
+                            <TextWithTooltip
+                              text="Applicant"
+                              tooltip="Legal Business Name from SAM.gov for this UEI"
+                            />
+                          </th>
                         </tr>
-                      ))
-                    )}
-                  </tbody>
-                </table>
-              </div>
+                      </thead>
+                      <tbody>
+                        {activeSamRecords.map((samData, index) => (
+                          <tr key={index}>
+                            <th scope="row" className="font-sans-2xs">
+                              <button
+                                className="usa-button font-sans-2xs margin-right-0 padding-x-105 padding-y-1"
+                                onClick={(ev) => {
+                                  setMessage({
+                                    displayed: true,
+                                    type: "info",
+                                    text: "Creating new rebate form application...",
+                                  });
+
+                                  createNewRebate(email, samData)
+                                    .then((res) => {
+                                      navigate(`/rebate/${res._id}`);
+                                    })
+                                    .catch((err) => {
+                                      setMessage({
+                                        displayed: true,
+                                        type: "error",
+                                        text: "Error creating new rebate form application.",
+                                      });
+                                    });
+                                }}
+                              >
+                                <span className="usa-sr-only">
+                                  Create Form with UEI:{" "}
+                                  {samData.UNIQUE_ENTITY_ID__c} and EFTI:{" "}
+                                  {samData.ENTITY_EFT_INDICATOR__c}
+                                </span>
+                                <span className="display-flex flex-align-center">
+                                  <svg
+                                    className="usa-icon"
+                                    aria-hidden="true"
+                                    focusable="false"
+                                    role="img"
+                                  >
+                                    <use href={`${icons}#arrow_forward`} />
+                                  </svg>
+                                  <span className="mobile-lg:display-none margin-left-1">
+                                    New Form
+                                  </span>
+                                </span>
+                              </button>
+                            </th>
+                            <td className="font-sans-2xs">
+                              {samData.UNIQUE_ENTITY_ID__c}
+                            </td>
+                            <td className="font-sans-2xs">
+                              {samData.ENTITY_EFT_INDICATOR__c}
+                            </td>
+                            <td className="font-sans-2xs">
+                              {samData.LEGAL_BUSINESS_NAME__c}
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </div>
+                </>
+              )}
             </div>
 
             <button
---
+++ app/client/src/utilities.tsx
@@ -0,0 +1,36 @@
+import { SamEntityData } from "contexts/user";
+
+/**
+ * Returns a user’s title and name when provided an email address and a SAM.gov
+ * entity/record.
+ */
+export function getUserInfo(email: string, samData: SamEntityData) {
+  const samEmailFields = [
+    "ELEC_BUS_POC_EMAIL__c",
+    "ALT_ELEC_BUS_POC_EMAIL__c",
+    "GOVT_BUS_POC_EMAIL__c",
+    "ALT_GOVT_BUS_POC_EMAIL__c",
+  ];
+
+  let matchedEmailField;
+
+  for (const [field, value] of Object.entries(samData)) {
+    if (!samEmailFields.includes(field)) continue;
+    // NOTE: take the first match only (the assumption is if a user is listed
+    // as multiple POCs, their title and name will be the same for all POCs)
+    if (
+      typeof value === "string" &&
+      value.toLowerCase() === email.toLowerCase()
+    ) {
+      matchedEmailField = field;
+      break;
+    }
+  }
+
+  const fieldPrefix = matchedEmailField?.split("_EMAIL__c").shift();
+
+  return {
+    title: samData[`${fieldPrefix}_TITLE__c` as keyof SamEntityData] as string,
+    name: samData[`${fieldPrefix}_NAME__c` as keyof SamEntityData] as string,
+  };
+}
