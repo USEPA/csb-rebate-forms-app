73a898703b5ac719db1e3461491c08dd53417069
Courtney Myers <courtney.myers@erg.com>
2022-07-22 15:15:05 -0400

    Move client app's checking of csb enrollment closed status to get response from /api/epa-data, so client app doesn't need to be rebuilt for env variable to take effect

--- app/server/app/routes/api.js
+++ app/server/app/routes/api.js
@@ -97,6 +97,7 @@ router.get("/helpdesk-access", ensureHelpdesk, (req, res) => {
 router.get("/epa-data", (req, res) => {
   // Explicitly return only required attributes from user info
   res.json({
+    enrollmentClosed: process.env.CSB_ENROLLMENT_PERIOD === "closed",
     mail: req.user.mail,
     memberof: req.user.memberof,
     exp: req.user.exp,
