3694d2d3fc6c1542fad55ff27379293dcb1babab
Courtney Myers <courtney.myers@erg.com>
2022-09-01 11:40:28 -0400

    Update capitalization to be consistent in messages

--- app/client/src/config.tsx
+++ app/client/src/config.tsx
@@ -61,7 +61,7 @@ export const messages = {
   newApplication:
     "Please select the “New Application” button above to create your first rebate application.",
   helpdeskApplicationSubmissionError:
-    "Error loading application form submission. Please confirm the Application ID is correct and search again.",
+    "Error loading Application form submission. Please confirm the Application ID is correct and search again.",
   timeout:
     "For security reasons, you have been logged out due to 15 minutes of inactivity.",
   logout: "You have successfully logged out.",
