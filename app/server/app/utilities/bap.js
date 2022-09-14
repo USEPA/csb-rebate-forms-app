a129cef42e491e91ad2f3a7759e17c6a4bac5ae1
Courtney Myers <courtney.myers@erg.com>
2022-09-13 21:17:50 -0400

    Log BAP queries, simplify SOQL comments, and refactor code used to verify bap connection before querying BAP

--- app/server/app/utilities/bap.js
+++ app/server/app/utilities/bap.js
@@ -76,8 +76,8 @@ function setupConnection(app) {
 
   return bapConnection
     .loginByOAuth2(BAP_USER, BAP_PASSWORD)
-    .then(() => {
-      const message = `Initializing BAP Connection.`;
+    .then((userInfo) => {
+      const message = `Initializing BAP Connection: ${userInfo.url}.`;
       log({ level: "info", message });
       // Store bapConnection in global express object using app.locals
       app.locals.bapConnection = bapConnection;
@@ -91,56 +91,48 @@ function setupConnection(app) {
  * Uses cached JSforce connection to query the BAP for SAM.gov entities.
  * @param {string} email
  * @param {express.Request} req
- * @returns {Promise<BapSamEntity[]>} collection of SAM.gov entities
+ * @returns {Promise<BapSamEntity[]>} collection of SAM.gov entity data
  */
 async function queryForSamEntities(email, req) {
+  const message = `Querying BAP for SAM.gov entities for user with email: ${email}.`;
+  log({ level: "info", message });
+
   /** @type {jsforce.Connection} */
   const bapConnection = req.app.locals.bapConnection;
 
   /* SOQL: */
-  // return bapConnection
-  //   .query(
-  //     `
-  //       SELECT
-  //         ENTITY_COMBO_KEY__c,
-  //         ENTITY_STATUS__c,
-  //         UNIQUE_ENTITY_ID__c,
-  //         ENTITY_EFT_INDICATOR__c,
-  //         CAGE_CODE__c,
-  //         LEGAL_BUSINESS_NAME__c,
-  //         GOVT_BUS_POC_NAME__c,
-  //         GOVT_BUS_POC_EMAIL__c,
-  //         GOVT_BUS_POC_TITLE__c,
-  //         ALT_GOVT_BUS_POC_NAME__c,
-  //         ALT_GOVT_BUS_POC_EMAIL__c,
-  //         ALT_GOVT_BUS_POC_TITLE__c,
-  //         ELEC_BUS_POC_NAME__c,
-  //         ELEC_BUS_POC_EMAIL__c,
-  //         ELEC_BUS_POC_TITLE__c,
-  //         ALT_ELEC_BUS_POC_NAME__c,
-  //         ALT_ELEC_BUS_POC_EMAIL__c,
-  //         ALT_ELEC_BUS_POC_TITLE__c,
-  //         PHYSICAL_ADDRESS_LINE_1__c,
-  //         PHYSICAL_ADDRESS_LINE_2__c,
-  //         PHYSICAL_ADDRESS_CITY__c,
-  //         PHYSICAL_ADDRESS_PROVINCE_OR_STATE__c,
-  //         PHYSICAL_ADDRESS_ZIPPOSTAL_CODE__c,
-  //         PHYSICAL_ADDRESS_ZIP_CODE_4__c
-  //       FROM
-  //         ${BAP_SAM_TABLE}
-  //       WHERE
-  //         ALT_ELEC_BUS_POC_EMAIL__c = '${email}' OR
-  //         GOVT_BUS_POC_EMAIL__c = '${email}' OR
-  //         ALT_GOVT_BUS_POC_EMAIL__c = '${email}' OR
-  //         ELEC_BUS_POC_EMAIL__c = '${email}'
-  //     `
-  //   )
-  //   .then((res) => {
-  //     return res.records;
-  //   })
-  //   .catch((err) => {
-  //     throw err;
-  //   });
+  // `SELECT
+  //   ENTITY_COMBO_KEY__c,
+  //   ENTITY_STATUS__c,
+  //   UNIQUE_ENTITY_ID__c,
+  //   ENTITY_EFT_INDICATOR__c,
+  //   CAGE_CODE__c,
+  //   LEGAL_BUSINESS_NAME__c,
+  //   GOVT_BUS_POC_NAME__c,
+  //   GOVT_BUS_POC_EMAIL__c,
+  //   GOVT_BUS_POC_TITLE__c,
+  //   ALT_GOVT_BUS_POC_NAME__c,
+  //   ALT_GOVT_BUS_POC_EMAIL__c,
+  //   ALT_GOVT_BUS_POC_TITLE__c,
+  //   ELEC_BUS_POC_NAME__c,
+  //   ELEC_BUS_POC_EMAIL__c,
+  //   ELEC_BUS_POC_TITLE__c,
+  //   ALT_ELEC_BUS_POC_NAME__c,
+  //   ALT_ELEC_BUS_POC_EMAIL__c,
+  //   ALT_ELEC_BUS_POC_TITLE__c,
+  //   PHYSICAL_ADDRESS_LINE_1__c,
+  //   PHYSICAL_ADDRESS_LINE_2__c,
+  //   PHYSICAL_ADDRESS_CITY__c,
+  //   PHYSICAL_ADDRESS_PROVINCE_OR_STATE__c,
+  //   PHYSICAL_ADDRESS_ZIPPOSTAL_CODE__c,
+  //   PHYSICAL_ADDRESS_ZIP_CODE_4__c
+  // FROM
+  //   ${BAP_SAM_TABLE}
+  // WHERE
+  //   ALT_ELEC_BUS_POC_EMAIL__c = '${email}' OR
+  //   GOVT_BUS_POC_EMAIL__c = '${email}' OR
+  //   ALT_GOVT_BUS_POC_EMAIL__c = '${email}' OR
+  //   ELEC_BUS_POC_EMAIL__c = '${email}'`
 
   return await bapConnection
     .sobject(BAP_SAM_TABLE)
@@ -187,38 +179,30 @@ async function queryForSamEntities(email, req) {
  * Uses cached JSforce connection to query the BAP for application form submissions.
  * @param {string[]} comboKeys
  * @param {express.Request} req
- * @returns {Promise<BapApplicationSubmission[]>} collection of application form submissions
+ * @returns {Promise<BapApplicationSubmission[]>} collection of fields associated with each application form submission
  */
 async function queryForApplicationSubmissions(comboKeys, req) {
+  const message = `Querying BAP for Application form submissions associated with combokeys: ${comboKeys}.`;
+  log({ level: "info", message });
+
   /** @type {jsforce.Connection} */
   const bapConnection = req.app.locals.bapConnection;
 
   /* SOQL: */
-  // return bapConnection
-  //   .query(
-  //     `
-  //       SELECT
-  //         CSB_Form_ID__c,
-  //         CSB_Modified_Full_String__c,
-  //         CSB_Review_Item_ID__c,
-  //         Parent_Rebate_ID__c,
-  //         Parent_CSB_Rebate__r.CSB_Rebate_Status__c
-  //       FROM
-  //         ${BAP_FORMS_TABLE}
-  //       WHERE
-  //         ${comboKeys
-  //           .map((key) => `UEI_EFTI_Combo_Key__c = '${key}'`)
-  //           .join(" OR ")}
-  //       ORDER BY
-  //         CreatedDate DESC
-  //     `
-  //   )
-  //   .then((res) => {
-  //     return res.records;
-  //   })
-  //   .catch((err) => {
-  //     throw err;
-  //   });
+  // `SELECT
+  //   CSB_Form_ID__c,
+  //   CSB_Modified_Full_String__c,
+  //   CSB_Review_Item_ID__c,
+  //   Parent_Rebate_ID__c,
+  //   Parent_CSB_Rebate__r.CSB_Rebate_Status__c
+  // FROM
+  //   ${BAP_FORMS_TABLE}
+  // WHERE
+  //   ${comboKeys
+  //     .map((key) => `UEI_EFTI_Combo_Key__c = '${key}'`)
+  //     .join(" OR ")}
+  // ORDER BY
+  //   CreatedDate DESC`
 
   return await bapConnection
     .sobject(BAP_FORMS_TABLE)
@@ -238,18 +222,21 @@ async function queryForApplicationSubmissions(comboKeys, req) {
 }
 
 /**
- * Fetches SAM.gov entities associated with a provided user.
- * @param {string} email
+ * Verifies the BAP connection has been setup, then calls the provided callback function with the provided arguments.
  * @param {express.Request} req
+ * @param {Object} callback callback function name and arguments to call after BAP connection has been verified
+ * @param {function} callback.name name of the callback function
+ * @param {any[]} callback.args arguments to pass to the callback function
  */
-function getSamEntities(email, req) {
-  // Make sure BAP connection has been initialized
+function verifyBapConnection(req, { name, args }) {
+  // BAP connection has not yet been initialized, so set set it up and call
+  // callback function
   if (!req.app.locals.bapConnection) {
     const message = `BAP Connection has not yet been initialized.`;
     log({ level: "info", message });
 
     return setupConnection(req.app)
-      .then(() => queryForSamEntities(email, req))
+      .then(() => name(...args))
       .catch((err) => {
         const message = `BAP Error: ${err}`;
         log({ level: "error", message, req });
@@ -257,7 +244,10 @@ function getSamEntities(email, req) {
       });
   }
 
-  return queryForSamEntities(email, req).catch((err) => {
+  // BAP connection has already been initialized, so call callback function
+  return name(...args).catch((err) => {
+    // in case of errors, log appropriate error, attempt to setup connection
+    // once more, and then call callback function
     if (err?.toString() === "invalid_grant: expired access/refresh token") {
       const message = `BAP access token expired`;
       log({ level: "info", message, req });
@@ -267,12 +257,24 @@ function getSamEntities(email, req) {
     }
 
     return setupConnection(req.app)
-      .then(() => queryForSamEntities(email, req))
+      .then(() => name(...args))
       .catch((retryErr) => {
         const message = `BAP Error: ${retryErr}`;
         log({ level: "error", message, req });
         throw retryErr;
       });
+  });
+}
+
+/**
+ * Fetches SAM.gov entities associated with a provided user.
+ * @param {string} email
+ * @param {express.Request} req
+ */
+function getSamEntities(email, req) {
+  return verifyBapConnection(req, {
+    name: queryForSamEntities,
+    args: [email, req],
   });
 }
 
@@ -295,36 +297,9 @@ function getBapComboKeys(email, req) {
  * @param {express.Request} req
  */
 function getApplicationSubmissions(comboKeys, req) {
-  // Make sure BAP connection has been initialized
-  if (!req.app.locals.bapConnection) {
-    const message = `BAP Connection has not yet been initialized.`;
-    log({ level: "info", message });
-
-    return setupConnection(req.app)
-      .then(() => queryForApplicationSubmissions(comboKeys, req))
-      .catch((err) => {
-        const message = `BAP Error: ${err}`;
-        log({ level: "error", message, req });
-        throw err;
-      });
-  }
-
-  return queryForApplicationSubmissions(comboKeys, req).catch((err) => {
-    if (err?.toString() === "invalid_grant: expired access/refresh token") {
-      const message = `BAP access token expired`;
-      log({ level: "info", message, req });
-    } else {
-      const message = `BAP Error: ${err}`;
-      log({ level: "error", message, req });
-    }
-
-    return setupConnection(req.app)
-      .then(() => queryForApplicationSubmissions(comboKeys, req))
-      .catch((retryErr) => {
-        const message = `BAP Error: ${retryErr}`;
-        log({ level: "error", message, req });
-        throw retryErr;
-      });
+  return verifyBapConnection(req, {
+    name: queryForApplicationSubmissions,
+    args: [comboKeys, req],
   });
 }
 
