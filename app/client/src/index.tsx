8b6b4d87e241303388735ab36cc981448f4d1fe8
Courtney Myers <courtney.myers@erg.com>
2023-04-27 16:20:04 -0400

    Update app to support react-query devtools in production

--- app/client/src/index.tsx
+++ app/client/src/index.tsx
@@ -1,7 +1,6 @@
-import { StrictMode } from "react";
+import { Suspense, StrictMode, lazy, useState, useEffect } from "react";
 import { render } from "react-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import reportWebVitals from "./reportWebVitals";
 // ---
 import { ErrorBoundary } from "components/errorBoundary";
@@ -11,25 +10,52 @@ import { NotificationsProvider } from "contexts/notifications";
 import "./tailwind-preflight.css";
 import "./styles.css";
 
-const queryClient = new QueryClient();
+declare global {
+  interface Window {
+    csb: any;
+  }
+}
+
 const container = document.getElementById("root") as HTMLElement;
 
-render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ErrorBoundary>
-        <DialogProvider>
-          <NotificationsProvider>
-            <App />
-          </NotificationsProvider>
-        </DialogProvider>
-      </ErrorBoundary>
+const queryClient = new QueryClient();
 
-      <ReactQueryDevtools />
-    </QueryClientProvider>
-  </StrictMode>,
-  container
+const ReactQueryDevtoolsProduction = lazy(() =>
+  import("@tanstack/react-query-devtools/build/lib/index.prod.js").then(
+    (module) => ({ default: module.ReactQueryDevtools })
+  )
 );
+
+function Index() {
+  const [devtoolsDisplayed, setDevtoolsDisplayed] = useState(false);
+
+  useEffect(() => {
+    window.csb ??= {};
+    window.csb.toggleDevtools = () => setDevtoolsDisplayed((value) => !value);
+  });
+
+  return (
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <ErrorBoundary>
+          <DialogProvider>
+            <NotificationsProvider>
+              <App />
+            </NotificationsProvider>
+          </DialogProvider>
+        </ErrorBoundary>
+
+        {devtoolsDisplayed && (
+          <Suspense fallback={null}>
+            <ReactQueryDevtoolsProduction />
+          </Suspense>
+        )}
+      </QueryClientProvider>
+    </StrictMode>
+  );
+}
+
+render(<Index />, container);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
