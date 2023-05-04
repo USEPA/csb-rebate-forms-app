71c4aa4b10f83db9f70a9e7e944f29a25fd9315c
Courtney Myers <courtney.myers@erg.com>
2023-05-04 14:34:04 -0400

    Update server index file documentation and formatting

--- app/server/app/index.js
+++ app/server/app/index.js
@@ -18,7 +18,16 @@ const {
 } = require("./middleware");
 const routes = require("./routes");
 
-const requiredEnvVars = [
+const {
+  NODE_ENV,
+  PORT,
+  CLIENT_URL,
+  SERVER_BASE_PATH,
+  CLOUD_SPACE,
+  JSON_PAYLOAD_LIMIT,
+} = process.env;
+
+const requiredEnvironmentVariables = [
   "SERVER_URL",
   "SAML_LOGIN_URL",
   "SAML_LOGOUT_URL",
@@ -40,16 +49,17 @@ const requiredEnvVars = [
   "S3_PUBLIC_REGION",
 ];
 
-requiredEnvVars.forEach((envVar) => {
-  if (!process.env[envVar]) {
-    const message = `Required environment variable ${envVar} not found.`;
-    log({ level: "error", message });
+requiredEnvironmentVariables.forEach((variable) => {
+  if (!process.env[variable]) {
+    const logMessage = `Required environment variable ${variable} not found.`;
+    log({ level: "error", message: logMessage });
+
     process.exitCode = 1;
   }
 });
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = PORT || 3001;
 
 app.use(
   helmet({
@@ -59,13 +69,10 @@ app.use(
 );
 app.use(helmet.hsts({ maxAge: 31536000 }));
 
-// Instruct web browsers to disable caching
+/** Instruct web browsers to disable caching. */
 app.use((req, res, next) => {
   res.setHeader("Surrogate-Control", "no-store");
-  res.setHeader(
-    "Cache-Control",
-    "no-store, no-cache, must-revalidate, proxy-revalidate"
-  );
+  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate"); // prettier-ignore
   res.setHeader("Pragma", "no-cache");
   res.setHeader("Expires", "0");
   next();
@@ -73,49 +80,59 @@ app.use((req, res, next) => {
 
 app.disable("x-powered-by");
 
-// Enable CORS and logging with morgan for local development only
-// NOTE: process.env.NODE_ENV set to "development" below to match value defined
-// in create-react-app when client app is run locally via `npm start`
-if (process.env.NODE_ENV === "development") {
-  app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+/**
+ * Enable CORS and logging with morgan for local development only.
+ * NOTE: process.env.NODE_ENV set to "development" below to match value defined
+ * in create-react-app when client app is run locally via `npm start`
+ */
+if (NODE_ENV === "development") {
+  app.use(cors({ origin: CLIENT_URL, credentials: true }));
   app.use(morgan("dev"));
 }
 
-// Apply global middleware on dev/staging in order for scan to receive 200 status on all endpoints
-if (
-  process.env.CLOUD_SPACE === "dev" ||
-  process.env.CLOUD_SPACE === "staging"
-) {
+/**
+ * Apply global middleware on dev/staging in order for scan to receive 200
+ * status on all endpoints
+ */
+if (CLOUD_SPACE === "dev" || CLOUD_SPACE === "staging") {
   app.use(appScan);
 }
 
-app.use(express.json({ limit: process.env.JSON_PAYLOAD_LIMIT || "5mb" }));
+app.use(express.json({ limit: JSON_PAYLOAD_LIMIT || "5mb" }));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(passport.initialize());
 passport.use("saml", samlStrategy);
 
-// If SERVER_BASE_PATH is provided, serve routes and static files from there (e.g. /csb)
-const basePath = `${process.env.SERVER_BASE_PATH || ""}/`;
+/**
+ * If SERVER_BASE_PATH is provided, serve routes and static files from there
+ * (e.g. /csb).
+ */
+const basePath = `${SERVER_BASE_PATH || ""}/`;
 app.use(basePath, routes);
 
-// Use regex to add trailing slash on static requests (required when using sub path)
-const pathRegex = new RegExp(`^\\${process.env.SERVER_BASE_PATH || ""}$`);
+/**
+ * Use regex to add trailing slash on static requests
+ * (required when using sub path).
+ */
+const pathRegex = new RegExp(`^\\${SERVER_BASE_PATH || ""}$`);
 app.all(pathRegex, (req, res) => res.redirect(`${basePath}`));
 
-// Serve client app's static built files
-// NOTE: client app's `build` directory contents copied into server app's
-// `public` directory in CI/CD step
+/**
+ * Serve client app's static built files.
+ * NOTE: client app's `build` directory contents copied into server app's
+ * `public` directory in CI/CD step.
+ */
 app.use(basePath, express.static(resolve(__dirname, "public")));
 
-// Ensure that requested client route exists (otherwise send 404)
+/** Ensure that requested client route exists (otherwise send 404). */
 app.use(checkClientRouteExists);
 
-// Ensure user is authenticated on all client-side routes except / and /welcome
+/** Ensure user is authenticated on all client-side routes except / and /welcome */
 app.use(protectClientRoutes);
 
-// Serve client-side routes
+/** Serve client-side routes. */
 app.get("*", (req, res) => {
   res.sendFile(resolve(__dirname, "public/index.html"));
 });
@@ -123,5 +140,6 @@ app.get("*", (req, res) => {
 app.use(errorHandler);
 
 app.listen(port, () => {
-  log({ level: "info", message: `Server listening on port ${port}` });
+  const logMessage = `Server listening on port ${port}`;
+  log({ level: "info", message: logMessage });
 });
