dfce2bd8cc4cfe7edaa219f9488d0579dc134ee6
Courtney Myers <courtney.myers@erg.com>
2023-05-04 10:17:58 -0400

    Update middleware functions documentation and formatting

--- app/server/app/middleware.js
+++ app/server/app/middleware.js
@@ -7,57 +7,68 @@ const { createJwt, jwtAlgorithm } = require("./utilities/createJwt");
 const log = require("./utilities/logger");
 const { getBapComboKeys } = require("./utilities/bap");
 
+const { CLIENT_URL, SERVER_URL, SERVER_BASE_PATH, JWT_PUBLIC_KEY } =
+  process.env;
+
 const cookieName = "csb-token";
 
 /**
- * Middleware to check for JWT, add user object to request, and create new JWT to keep alive for 15 minutes from request
- * Default to rejectRequest function if jwt is invalid, but allow for a custom override function on reject
- * (required for auto-redirect to SAML)
+ * Middleware to check for JWT, add user object to request, and create new JWT
+ * to keep alive for 15 minutes from request. Default to rejectRequest function
+ * if JWT is invalid, but allow for a custom override function on reject
+ * (required for auto-redirect to SAML).
+ *
  * @param {express.Request} req
  * @param {express.Response} res
  * @param {express.NextFunction} next
  */
 function ensureAuthenticated(req, res, next, rejectCallback = rejectRequest) {
-  // If no JWT passed in token cookie, send Unauthorized response or redirect
+  /** If no JWT passed in token cookie, send Unauthorized response or redirect. */
   if (!req.cookies[cookieName]) {
-    log({ level: "warn", message: "No jwt cookie present in request", req });
+    const logMessage = `No JWT cookie present in request.`;
+    log({ level: "warn", message: logMessage, req });
+
     return rejectCallback(req, res);
   }
 
   jwt.verify(
     req.cookies[cookieName],
-    process.env.JWT_PUBLIC_KEY,
+    JWT_PUBLIC_KEY,
     { algorithms: [jwtAlgorithm] },
     function (err, user) {
       if (err) {
-        // Change log levels depending on jwt error received
-        if (err instanceof jwt.TokenExpiredError) {
-          log({ level: "warn", message: "JWT expired.", req });
+        const jwtExpired = err instanceof jwt.TokenExpiredError;
+
+        /** Change log levels depending on JWT error received. */
+        if (jwtExpired) {
+          const logMessage = `JWT expired.`;
+          log({ level: "warn", message: logMessage, req });
         } else if (err instanceof jwt.JsonWebTokenError) {
-          log({ level: "error", message: "An invalid JWT was used.", req });
+          const logMessage = `An invalid JWT was used.`;
+          log({ level: "error", message: logMessage, req });
         } else {
-          const message =
-            typeof err.toString === "function" ? err.toString() : err;
-          log({ level: "error", message, req });
+          const logMessage = typeof err.toString === "function" ? err.toString() : err; // prettier-ignore
+          log({ level: "error", message: logMessage, req });
         }
 
-        // if err is TokenExpiredError, expired will be true and user will see inactive message instead of error
-        return rejectCallback(req, res, err instanceof jwt.TokenExpiredError);
+        /** If JWT has expired, user will see inactivity message instead of error. */
+        return rejectCallback(req, res, jwtExpired);
       }
 
-      // Add user to the request object
+      /** Add user to the request object. */
       req.user = user;
 
-      // Create new token to update expiration to 15 min from now
-      const newToken = createJwt(user);
+      /** Create a new token to update expiration to 15 min from now. */
+      const token = createJwt(user);
 
-      // Add JWT in cookie and proceed with request
-      res.cookie(cookieName, newToken, {
+      /** Add JWT in cookie and proceed with request. */
+      res.cookie(cookieName, token, {
         httpOnly: true,
         overwrite: true,
         sameSite: "lax",
         secure: true,
       });
+
       next();
     }
   );
@@ -65,24 +76,24 @@ function ensureAuthenticated(req, res, next, rejectCallback = rejectRequest) {
 
 /**
  * Confirm user has either "csb_admin" or "csb_helpdesk" role.
- * Log message and send 401 Unauthorized if user does not have either role
+ * Log message and send 401 Unauthorized if user does not have either role.
+ *
  * @param {express.Request} req
  * @param {express.Response} res
  * @param {express.NextFunction} next
  */
 function ensureHelpdesk(req, res, next) {
-  const userRoles = req.user.memberof ? req.user.memberof.split(",") : [];
+  const { mail, memberof } = req.user;
+  const userRoles = memberof?.split(",") || [];
 
   if (!userRoles.includes("csb_admin") && !userRoles.includes("csb_helpdesk")) {
     if (!req.originalUrl.includes("/helpdesk-access")) {
-      log({
-        level: "error",
-        message: `User with email ${req.user.mail} attempted to perform an admin/helpdesk action without correct privileges.`,
-        req,
-      });
+      const logMessage = `User with email ${mail} attempted to perform an admin/helpdesk action without correct privileges.`;
+      log({ level: "error", message: logMessage, req });
     }
 
-    return res.status(401).json({ message: "Unauthorized" });
+    const errorMessage = `Unauthorized.`;
+    return res.status(401).json({ message: errorMessage });
   }
 
   next();
@@ -91,47 +102,52 @@ function ensureHelpdesk(req, res, next) {
 /**
  * @param {express.Request} req
  * @param {express.Response} res
- * @param {boolean} expired
+ * @param {boolean} jwtExpired
  */
-function rejectRequest(req, res, expired) {
-  // Clear token cookie if there was an error verifying (e.g. expired)
+function rejectRequest(req, res, jwtExpired) {
+  /** Clear token cookie if there was an error verifying (e.g. jwtExpired). */
   res.clearCookie(cookieName);
 
   if (req.originalUrl.includes("/api")) {
-    // Send JSON Unauthorized message if request is for an API endpoint
-    return res.status(401).json({ message: "Unauthorized" });
+    /** Send JSON Unauthorized message if request is for an API endpoint. */
+    const errorMessage = `Unauthorized.`;
+    return res.status(401).json({ message: errorMessage });
   }
-  // For non-API requests (e.g. on logout), redirect to front-end if token is non-existent or invalid
-  // If expired, display timeout info message instead of auth error
-  return res.redirect(
-    `${process.env.CLIENT_URL || process.env.SERVER_URL}/welcome?${
-      expired ? "info=timeout" : "error=auth"
-    }`
-  );
+
+  /**
+   * For non-API requests (e.g. on logout), redirect to /welcome if token is
+   * non-existent or invalid, and display the appropriate message (timeout or
+   * authentication error).
+   */
+  const param = jwtExpired ? "info=timeout" : "error=auth";
+  return res.redirect(`${CLIENT_URL || SERVER_URL}/welcome?${param}`);
 }
 
 /**
- * Auto-redirect to SAML login for any non-logged-in user on any route except base "/" or "/welcome"
+ * Auto-redirect to SAML login for any non-logged-in user on any route except
+ * base "/" or "/welcome".
+ *
  * @param {express.Request} req
  * @param {express.Response} res
  * @param {express.NextFunction} next
  */
 function protectClientRoutes(req, res, next) {
-  const subPath = process.env.SERVER_BASE_PATH || "";
+  const subPath = SERVER_BASE_PATH || "";
   const unprotectedRoutes = ["/", "/welcome", "/manifest.json"].map(
     (route) => `${subPath}${route}`
   );
+
   if (!unprotectedRoutes.includes(req.path) && !req.path.includes("/static")) {
     return ensureAuthenticated(req, res, next, (req, res) => {
-      // If ensureAuthenticated does not find valid jwt, this redirect will occur so user is auto-redirected to SAML
-      return res.redirect(
-        `${process.env.SERVER_URL}/login?RelayState=${req.originalUrl.replace(
-          subPath,
-          ""
-        )}`
-      );
+      /**
+       * If ensureAuthenticated does not find valid JWT, this redirect will
+       * occur so user is auto-redirected to SAML.
+       */
+      const url = req.originalUrl.replace(subPath, "");
+      return res.redirect(`${SERVER_URL}/login?RelayState=${url}`);
     });
   }
+
   next();
 }
 
@@ -141,10 +157,11 @@ function protectClientRoutes(req, res, next) {
  * @param {express.NextFunction} next
  */
 function checkClientRouteExists(req, res, next) {
-  const subPath = process.env.SERVER_BASE_PATH || "";
+  const subPath = SERVER_BASE_PATH || "";
   const clientRoutes = ["/", "/welcome", "/helpdesk", "/rebate/new"].map(
     (route) => `${subPath}${route}`
   );
+
   if (
     !clientRoutes.includes(req.path) &&
     !req.path.includes("/rebate/") &&
@@ -153,38 +170,46 @@ function checkClientRouteExists(req, res, next) {
   ) {
     return res.status(404).sendFile(resolve(__dirname, "public/404.html"));
   }
+
   next();
 }
 
 /**
- * Global middleware on dev/staging to send 200 status on all server endpoints (required for ZAP scan)
+ * Global middleware on dev/staging to send 200 status on all server endpoints
+ * (required for ZAP scan).
+ *
  * @param {express.Request} req
  * @param {express.Response} res
  * @param {express.NextFunction} next
  */
 function appScan(req, res, next) {
-  // OpenAPI def must use global "scan" param and enum to "true"
+  /** OpenAPI def must use global "scan" param and enum to "true". */
   if (req.query.scan === "true") {
     return res.json({ status: 200 });
   }
+
   next();
 }
 
 /**
  * Fetch user's SAM.gov unique combo keys from the BAP and add "bapComboKeys"
  * to request object if successful.
+ *
  * @param {express.Request} req
  * @param {express.Response} res
  * @param {express.NextFunction} next
  */
 function storeBapComboKeys(req, res, next) {
-  getBapComboKeys(req, req.user.mail)
+  const { mail } = req.user;
+
+  getBapComboKeys(req, mail)
     .then((bapComboKeys) => {
       req.bapComboKeys = bapComboKeys;
       next();
     })
     .catch(() => {
-      return res.status(401).json({ message: "Error getting SAM.gov data" });
+      const errorMessage = `Error getting SAM.gov data.`;
+      return res.status(401).json({ message: errorMessage });
     });
 }
 
@@ -194,11 +219,11 @@ function storeBapComboKeys(req, res, next) {
  * @param {express.NextFunction} next
  */
 function verifyMongoObjectId(req, res, next) {
-  const id = req.params.id;
+  const { id } = req.params;
 
   if (id && !ObjectId.isValid(id)) {
-    const message = `MongoDB ObjectId validation error for: ${id}`;
-    return res.status(400).json({ message });
+    const errorMessage = `MongoDB ObjectId validation error for: ${id}.`;
+    return res.status(400).json({ message: errorMessage });
   }
 
   next();
