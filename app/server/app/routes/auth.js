bc9ca049388c6e964c65f099f1d6d167b7bf2449
Courtney Myers <courtney.myers@erg.com>
2023-05-05 15:42:18 -0400

    Update auth functions documentation and formatting

--- app/server/app/routes/auth.js
+++ app/server/app/routes/auth.js
@@ -5,10 +5,14 @@ const { ensureAuthenticated } = require("../middleware");
 const { createJWT, jwtCookieName } = require("../utilities/createJwt");
 const log = require("../utilities/logger");
 
+const { CLIENT_URL, SERVER_URL, SAML_PUBLIC_KEY } = process.env;
+
 const router = express.Router();
 
-// For redirects below, set const for base url (SERVER_URL is needed as fallback when using sub path, e.g. /csb)
-const baseUrl = process.env.CLIENT_URL || process.env.SERVER_URL;
+/**
+ * NOTE: SERVER_URL is needed as fallback when using sub path, e.g. /csb
+ */
+const baseUrl = CLIENT_URL || SERVER_URL;
 
 router.get(
   "/login",
@@ -26,81 +30,82 @@ router.post(
     session: false,
   }),
   (req, res) => {
-    // Create JWT, set as cookie, then redirect to client
-    // Note: nameID and nameIDFormat are required to send with logout request
+    const { body } = req;
+    const { attributes } = req.user;
+    const userGroups = attributes.memberof || "no";
+
+    /**
+     * Create JWT, set as cookie, then redirect to client.
+     * NOTE: nameID and nameIDFormat are required to send with logout request.
+     */
+
     const token = createJWT({
       ...req.user,
-      ...req.user.attributes,
+      ...attributes,
     });
+
     res.cookie(jwtCookieName, token, {
       httpOnly: true,
       sameSite: "lax",
       secure: true,
     });
 
-    // If user has Admin or Helpdesk role, log to INFO
-    log({
-      level: "info",
-      message: `User with email ${req.user.attributes.mail} and member of ${
-        req.user.attributes.memberof || "no"
-      } groups logged in.`,
-      req,
-    });
+    const logMessage = `User with email '${attributes.mail}' and member of ${userGroups} groups logged in.`;
+    log({ level: "info", message: logMessage, req });
 
-    // "RelayState" will be the path that the user initially tried to access before being sent to /login
-    res.redirect(`${baseUrl}${req.body.RelayState || "/"}`);
+    /**
+     * "RelayState" will be the path that the user initially tried to access
+     * before being sent to /login
+     */
+    res.redirect(`${baseUrl}${body.RelayState || "/"}`);
   }
 );
 
 router.get("/login/fail", (req, res) => {
-  log({
-    level: "error",
-    message: "SAML Error - Login failed",
-    req,
-  });
+  const logMessage = `SAML Error - Login failed.`;
+  log({ level: "error", message: logMessage, req });
+
   res.redirect(`${baseUrl}/welcome?error=saml`);
 });
 
 router.get("/logout", ensureAuthenticated, (req, res) => {
   samlStrategy.logout(req, function (err, requestUrl) {
     if (err) {
-      log({
-        level: "error",
-        message: `SAML Error - Passport logout failed - ${err}`,
-        req,
-      });
+      const logMessage = `SAML Error - Passport logout failed - ${err}`;
+      log({ level: "error", message: logMessage, req });
+
       res.redirect(`${baseUrl}/`);
     } else {
-      // Send request to SAML logout url
+      /** Send request to SAML logout url. */
       res.redirect(requestUrl);
     }
   });
 });
 
 const logoutCallback = (req, res) => {
-  // Clear token cookie so client no longer passes JWT after logout
+  /** Clear token cookie so client no longer passes JWT after logout. */
   res.clearCookie(jwtCookieName);
 
-  // If "RelayState" was passed in original logout request (either querystring or post body), redirect to below
-  const RelayState = req.query?.RelayState || req.body?.RelayState;
-  res.redirect(`${baseUrl}${RelayState || "/welcome?success=logout"}`);
+  /**
+   * If "RelayState" was passed in original logout request (either querystring
+   * or post body), redirect to below.
+   */
+  const relayState = req.query?.RelayState || req.body?.RelayState;
+  res.redirect(`${baseUrl}${relayState || "/welcome?success=logout"}`);
 };
 
-// Local saml config sends GET for logout callback, while EPA config sends POST. Handle both
+/**
+ * Local SAML config sends GET for logout callback, while EPA config sends POST.
+ */
 router.get("/logout/callback", logoutCallback);
 router.post("/logout/callback", logoutCallback);
 
-// Return SAML metadata
+/** Return SAML metadata. */
 router.get("/metadata", (req, res) => {
   res.type("application/xml");
   res
     .status(200)
-    .send(
-      samlStrategy.generateServiceProviderMetadata(
-        null,
-        process.env.SAML_PUBLIC_KEY
-      )
-    );
+    .send(samlStrategy.generateServiceProviderMetadata(null, SAML_PUBLIC_KEY));
 });
 
 module.exports = router;
