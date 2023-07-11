f9833c0137496b26f302a314ea89259c8c9c9ecb
Courtney Myers <courtney.myers@erg.com>
2023-07-11 12:40:46 -0400

    Move new application button out of dashboard component (will go on all rebates page) and update styles (shrink buttons, update small screen layout)

--- app/client/src/components/userDashboard.tsx
+++ app/client/src/components/userDashboard.tsx
@@ -5,12 +5,7 @@ import uswds from "@formio/uswds";
 import icons from "uswds/img/sprite.svg";
 // ---
 import { serverUrlForHrefs, formioBaseUrl, formioProjectUrl } from "../config";
-import {
-  useCsbQuery,
-  useBapSamQuery,
-  useCsbData,
-  useBapSamData,
-} from "../utilities";
+import { useCsbQuery, useBapSamQuery, useBapSamData } from "../utilities";
 import { useHelpdeskAccess } from "components/app";
 import { Loading } from "components/loading";
 import { useDialogActions } from "contexts/dialog";
@@ -27,6 +22,8 @@ function IconText(props: {
 }) {
   const { order, icon, text } = props;
 
+  const alignment = order === "icon-text" ? "left" : "right";
+
   const Icon = (
     <svg
       key="icon"
@@ -40,10 +37,7 @@ function IconText(props: {
   );
 
   const Text = (
-    <span
-      key="text"
-      className={`margin-${order === "icon-text" ? "left" : "right"}-1`}
-    >
+    <span key="text" className={`margin-${alignment}-1 text-${alignment}`}>
       {text}
     </span>
   );
@@ -64,7 +58,6 @@ export function UserDashboard(props: { email: string }) {
   useCsbQuery();
   useBapSamQuery();
 
-  const csbData = useCsbData();
   const bapSamData = useBapSamData();
 
   const { displayDialog } = useDialogActions();
@@ -72,15 +65,34 @@ export function UserDashboard(props: { email: string }) {
 
   const onAllRebatesPage = pathname === "/";
   const onHelpdeskPage = pathname === "/helpdesk";
+
   const onApplicationFormPage = pathname.startsWith("/rebate");
   const onPaymentRequestFormPage = pathname.startsWith("/payment-request");
   const onCloseOutFormPage = pathname.startsWith("/close-out");
 
-  const applicationFormOpen = csbData
-    ? csbData.submissionPeriodOpen.application
-    : false;
+  const onFormPage =
+    onApplicationFormPage || onPaymentRequestFormPage || onCloseOutFormPage;
 
-  if (!csbData || !bapSamData) {
+  const btnClassNames =
+    "usa-button margin-0 padding-x-2 padding-y-1 width-full font-sans-2xs";
+
+  function confirmationNavigation(destination: string) {
+    displayDialog({
+      dismissable: true,
+      heading: "Are you sure you want to navigate away from this page?",
+      description: (
+        <p>
+          If you haven’t saved the current form, any changes you’ve made will be
+          lost.
+        </p>
+      ),
+      confirmText: "Yes",
+      dismissText: "Cancel",
+      confirmedAction: () => navigate(destination),
+    });
+  }
+
+  if (!bapSamData) {
     return <Loading />;
   }
 
@@ -213,140 +225,63 @@ export function UserDashboard(props: { email: string }) {
       </div>
       */}
 
-      <div className="desktop:display-flex flex-justify border-bottom">
-        <nav className="desktop:order-last mobile-lg:display-flex flex-align-center flex-justify-end">
-          <p className="margin-bottom-1 margin-right-1">
-            <span>{email}</span>
-          </p>
+      <div className="flex-justify border-bottom tablet:display-flex ">
+        <nav className="flex-align-center mobile-lg:display-flex">
+          <div className="margin-bottom-1 mobile-lg:margin-right-1">
+            {onAllRebatesPage ? (
+              <button className={btnClassNames} disabled>
+                <IconText order="icon-text" icon="list" text="Dashboard" />
+              </button>
+            ) : (
+              <Link
+                to="/"
+                className={btnClassNames}
+                onClick={(ev) => {
+                  if (onFormPage) {
+                    ev.preventDefault();
+                    confirmationNavigation("/");
+                  }
+                }}
+              >
+                <IconText order="icon-text" icon="list" text="Dashboard" />
+              </Link>
+            )}
+          </div>
 
-          <a
-            className="margin-bottom-1 usa-button font-sans-2xs margin-right-0"
-            href={`${serverUrlForHrefs}/logout`}
-          >
-            <IconText order="text-icon" icon="logout" text="Sign out" />
-          </a>
-        </nav>
-
-        <nav>
-          {/* --- Your Rebate Forms --- */}
-          {onAllRebatesPage ? (
-            <button
-              className="margin-bottom-1 usa-button font-sans-2xs"
-              disabled
-            >
-              <IconText
-                order="icon-text"
-                icon="list"
-                text="Your Rebate Forms"
-              />
-            </button>
-          ) : (
-            <Link
-              to="/"
-              className="margin-bottom-1 usa-button font-sans-2xs"
-              onClick={(ev) => {
-                if (
-                  onApplicationFormPage ||
-                  onPaymentRequestFormPage ||
-                  onCloseOutFormPage
-                ) {
-                  ev.preventDefault();
-                  displayDialog({
-                    dismissable: true,
-                    heading:
-                      "Are you sure you want to navigate away from this page?",
-                    description: (
-                      <p>
-                        If you haven’t saved the current form, any changes
-                        you’ve made will be lost.
-                      </p>
-                    ),
-                    confirmText: "Yes",
-                    dismissText: "Cancel",
-                    confirmedAction: () => navigate("/"),
-                  });
-                }
-              }}
-            >
-              <IconText
-                order="icon-text"
-                icon="list"
-                text="Your Rebate Forms"
-              />
-            </Link>
-          )}
-
-          {/* --- New Application --- */}
-          {onApplicationFormPage ||
-          onPaymentRequestFormPage ||
-          onCloseOutFormPage ||
-          !applicationFormOpen ? (
-            <button
-              className="margin-bottom-1 usa-button font-sans-2xs"
-              disabled
-            >
-              <IconText
-                order="icon-text"
-                icon="add_circle"
-                text="New Application"
-              />
-            </button>
-          ) : (
-            <Link
-              to="/rebate/new"
-              className="margin-bottom-1 usa-button font-sans-2xs"
-            >
-              <IconText
-                order="icon-text"
-                icon="add_circle"
-                text="New Application"
-              />
-            </Link>
-          )}
-
-          {/* --- Helpdesk --- */}
           {helpdeskAccess === "success" && (
-            <>
+            <div className="margin-bottom-1 mobile-lg:margin-right-1">
               {onHelpdeskPage ? (
-                <button
-                  className="margin-bottom-1 usa-button font-sans-2xs"
-                  disabled
-                >
+                <button className={btnClassNames} disabled>
                   <IconText order="icon-text" icon="people" text="Helpdesk" />
                 </button>
               ) : (
                 <Link
                   to="/helpdesk"
-                  className="margin-bottom-1 usa-button font-sans-2xs"
+                  className={btnClassNames}
                   onClick={(ev) => {
-                    if (
-                      onApplicationFormPage ||
-                      onPaymentRequestFormPage ||
-                      onCloseOutFormPage
-                    ) {
+                    if (onFormPage) {
                       ev.preventDefault();
-                      displayDialog({
-                        dismissable: true,
-                        heading:
-                          "Are you sure you want to navigate away from this page?",
-                        description: (
-                          <p>
-                            If you haven’t saved the current form, any changes
-                            you’ve made will be lost.
-                          </p>
-                        ),
-                        confirmText: "Yes",
-                        dismissText: "Cancel",
-                        confirmedAction: () => navigate("/helpdesk"),
-                      });
+                      confirmationNavigation("/helpdesk");
                     }
                   }}
                 >
                   <IconText order="icon-text" icon="people" text="Helpdesk" />
                 </Link>
               )}
-            </>
+            </div>
           )}
+        </nav>
+
+        <nav className="flex-align-center mobile-lg:display-flex tablet:flex-justify-end">
+          <div className="margin-bottom-1">
+            <span className="font-sans-xs">{email}</span>
+          </div>
+
+          <div className="margin-bottom-1 mobile-lg:margin-left-1">
+            <a className={btnClassNames} href={`${serverUrlForHrefs}/logout`}>
+              <IconText order="text-icon" icon="logout" text="Sign out" />
+            </a>
+          </div>
         </nav>
       </div>
 
