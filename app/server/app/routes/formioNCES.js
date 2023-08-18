73eb82fc2681c752c33b580b7201452608ffbd50
Courtney Myers <courtney.myers@erg.com>
2023-08-18 14:08:52 -0400

    Comment out unused code for fetching JSON data in formioNCES server route

--- app/server/app/routes/formioNCES.js
+++ app/server/app/routes/formioNCES.js
@@ -1,12 +1,12 @@
-const { resolve } = require("node:path");
-const { readFile } = require("node:fs/promises");
+// const { resolve } = require("node:path");
+// const { readFile } = require("node:fs/promises");
 const express = require("express");
-const axios = require("axios").default || require("axios"); // TODO: https://github.com/axios/axios/issues/5011
+// const axios = require("axios").default || require("axios"); // TODO: https://github.com/axios/axios/issues/5011
 // ---
 const log = require("../utilities/logger");
 const data = require("../content/nces.json");
 
-const { NODE_ENV, S3_PUBLIC_BUCKET, S3_PUBLIC_REGION } = process.env;
+// const { NODE_ENV, S3_PUBLIC_BUCKET, S3_PUBLIC_REGION } = process.env;
 
 const router = express.Router();
 
@@ -14,7 +14,7 @@ const router = express.Router();
 router.get("/:searchText", async (req, res) => {
   const { searchText } = req.params;
 
-  const s3BucketUrl = `https://${S3_PUBLIC_BUCKET}.s3-${S3_PUBLIC_REGION}.amazonaws.com`;
+  // const s3BucketUrl = `https://${S3_PUBLIC_BUCKET}.s3-${S3_PUBLIC_REGION}.amazonaws.com`;
 
   // const data = JSON.parse(
   //   await (NODE_ENV === "development"
