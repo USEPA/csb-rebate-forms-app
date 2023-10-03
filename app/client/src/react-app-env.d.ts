5554ee7764559566d449338ec9a2df1bcda06449
Courtney Myers <courtney.myers@erg.com>
2023-10-03 18:10:49 -0400

    Remove no longer used react app type declaration file, reportWebVitals.ts, setupTests.ts, and update index.tsx's imports to include new @ alias import paths

--- app/client/src/react-app-env.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react-scripts" />
-
-declare module "formiojs/providers/storage/s3";
-declare module "@formio/react";
--- app/client/src/vite-env.d.ts
+++ app/client/src/vite-env.d.ts
@@ -1,1 +1,4 @@
 /// <reference types="vite/client" />
+
+// declare module "formiojs/providers/storage/s3";
+// declare module "@formio/react";
