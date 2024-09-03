31c1ba42393031949073d693c8e2f5d5dcd457b2
Courtney Myers <courtney.myers@erg.com>
2024-09-03 15:32:37 -0400

    Add CSB_REBATE_YEAR env variable and return its value in config API route

--- app/server/app/routes/config.js
+++ app/server/app/routes/config.js
@@ -3,13 +3,22 @@ const express = require("express");
 const { submissionPeriodOpen } = require("../config/formio");
 const { ensureAuthenticated } = require("../middleware");
 
+const { CSB_REBATE_YEAR } = process.env;
+
 const router = express.Router();
 
 router.use(ensureAuthenticated);
 
-// --- get CSB app specific configuration (form open enrollment status, etc.)
+// --- get CSB app specific configuration
 router.get("/", (req, res) => {
-  return res.json({ submissionPeriodOpen });
+  // NOTE: fallback to current year if CSB_REBATE_YEAR is not set
+  const date = new Date();
+  const year = date.getFullYear().toString();
+
+  return res.json({
+    rebateYear: CSB_REBATE_YEAR || year,
+    submissionPeriodOpen,
+  });
 });
 
 module.exports = router;
