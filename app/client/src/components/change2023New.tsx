c9dfe5592df23827f13fe942e272f69edf058fbd
Courtney Myers <courtney.myers@erg.com>
2026-08-27 11:12:42 -0400

    Update ChangeRequest2023Form component to include school district nces id field in submission data, and update FRF2023Submission, PRF2023Submission, and CRF2023Submission components to pass the field's data in their use of the ChangeRequest2023Button component

--- app/client/src/components/change2023New.tsx
+++ app/client/src/components/change2023New.tsx
@@ -31,6 +31,7 @@ type SubmissionData = {
   userTitle: string;
   userName: string;
   applicantName: string;
+  districtNcesId: string;
   districtName: string;
   districtState: string;
   districtPriority: string;
@@ -313,6 +314,7 @@ function ChangeRequest2023Form(props: {
               _user_title: data.userTitle,
               _user_name: data.userName,
               _bap_applicant_name: data.applicantName,
+              _bap_district_nces_id: data.districtNcesId,
               _bap_district_name: data.districtName,
               _bap_district_state: data.districtState,
               _bap_district_priority: data.districtPriority,
